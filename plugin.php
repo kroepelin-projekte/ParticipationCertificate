22879617f67abbe932ec39fdbf02ff8c09592110
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2026-07-09 15:32:48 +0200

    Updtae plugin version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 // alphanumerical ID of the plugin; never change this
 $id = 'dhbwparticipationpdf';
-$version = '2.1.1';
+$version = '2.1.2';
 $ilias_min_version = "10.0";
 $ilias_max_version = "10.999";
 $responsible = "Kröpelin Projekt GmbH ";
